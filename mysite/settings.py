ab5aa9d1ea8442ad6a1a92a9f48e4bd66a90abc7
iyed <abariyad01@gmail.com>
2021-09-15 21:16:25 +0100

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-et_v5!%x$xzki_7*8cc@0%f^uarl9)+7jbj!d*o@h_$q101$s2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['52.14.202.159','127.0.0.1']
+ALLOWED_HOSTS = ['52.14.202.159','127.0.0.1','ec2-52-14-202-159.us-east-2.compute.amazonaws.com']
 
 
 
